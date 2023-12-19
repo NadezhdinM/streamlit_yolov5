8c9f89512757ebd8b4cdd96b108aa1028a0dc230
Maksim Nadezhdin <maxim.nadezhdin@dalee.ru>
2023-12-19 22:16:09 +0300

    FEAT: Added more test

--- test_main.py
+++ test_main.py
@@ -12,9 +12,22 @@ def test_transform_image(mock_image):
     # Test the transform_image function
     transformed_image = transform_image(mock_image)
     assert transformed_image.shape == (1, 3, 224, 224)  # Assuming the shape returned by transform_image
+    assert transformed_image.max() <= 1.0  # Check if values are normalized
 
 def test_predict(mock_image):
     # Test the predict function
     transformed_image = transform_image(mock_image)
     prediction = predict(transformed_image)
     assert isinstance(prediction, int)  # Assuming predict returns an integer
+
+# Additional test cases for edge scenarios
+
+def test_transform_image_invalid_input():
+    # Test transform_image with invalid input (non-image)
+    with pytest.raises(AttributeError):
+        transform_image("invalid_input")
+
+def test_predict_invalid_input():
+    # Test predict with invalid input (non-tensor)
+    with pytest.raises(TypeError):
+        predict("invalid_input")
